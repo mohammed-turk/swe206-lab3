198c91f15cb1ee68145e7eaeeeb8effeeb52f69e
mtj66 <mtj6611@gmail.com>
2023-02-10 21:18:38 +0300

    added and implemented the selection sort method

--- Sorter.java
+++ Sorter.java
@@ -1,7 +1,24 @@
 public class Sorter{
-    
+        public static void SelactionSort(int[] list){
+        for (int i=0;i<list.length-1;i++){
+            int curreentMin=list[i];
+            int currentMinIndex= i;
+            for(int j=i+1;j<list.length;j++){
+                if (curreentMin>list[j]) {
+                    curreentMin = list[j];
+                    currentMinIndex=j;
+                }
+            }
+            if(curreentMin!= i){
+                list[currentMinIndex]=list[i];
+                list[i]=curreentMin;
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
-        
+        int[] x= {1,2,7,5};
+        SelactionSort(x);
     }
 }
