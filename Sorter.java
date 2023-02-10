0ad3466a02be508edc41c6109962b215edadd7c2
NBTON <s202040640@kfupm.edu.sa>
2023-02-10 21:31:29 +0300

    added the sort method to print the sorted array

--- Sorter.java
+++ Sorter.java
@@ -20,8 +20,16 @@ public class Sorter{
     public static void main(String[] args) {
         int[] x= {1,2,7,5};
         SelactionSort(x);
+
+
+        int arr[] = { 12, 11, 13, 5, 6 };
+        InsertionSort ob = new InsertionSort();
+        ob.insertionsort(arr);
+        ob.sort(arr);
     }
 }
+
+
 
  class InsertionSort {
     /*Function to sort array using insertion sort*/
@@ -32,9 +40,7 @@ public class Sorter{
             int key = arr[i];
             int j = i - 1;
  
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
+
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -42,4 +48,14 @@ public class Sorter{
             arr[j + 1] = key;
         }
     }
+
+     void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+ 
+        System.out.println();
+    }
+
 }
